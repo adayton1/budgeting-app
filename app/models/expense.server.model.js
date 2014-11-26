3234ade2d9e2308a26dbf448318ca5043802ae38
Alan Dayton <alan.dayton.1@gmail.com>
2014-11-26 09:53:18 -0700

    Added model for expenses

---
+++ app/models/expense.server.model.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Article Schema
+ */
+var ExpenseSchema = new Schema({
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	date: {
+		type: Date,
+		default: Date.now
+		required: 'Date cannot be blank'
+	},
+	location: {
+		type: String,
+		default: '',
+		trim: true,
+	},
+	amount: {
+		type: Number,
+		default: 0,
+		required: 'Amount cannot be blank'
+	},
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	}
+});
+
+mongoose.model('Expense', ExpenseSchema);
